cc0ef7f556ff262ea197aa0d98134cba7e90a04a
seohyun <tingko153@gmail.com>
2018-01-12 10:27:24 +0900

    88

--- vuejstest/my-app/src/main.js
+++ vuejstest/my-app/src/main.js
@@ -1,46 +1,63 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import App from './App';
 
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'hello vue!!!',
-    message2: 'hellow world!!'
-  }
-});
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     message: 'hello vue!!!',
+//     message2: 'hellow world!!'
+//   }
+// });
 
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: new Date()
-  }
-});
+// var app2 = new Vue({
+//   el: '#app-2',
+//   data: {
+//     message: new Date()
+//   }
+// });
 
-var app3 = new Vue({
-  el: '#app-3',
-  data: {
-    seen: true
-  }
-});
+// var app3 = new Vue({
+//   el: '#app-3',
+//   data: {
+//     seen: true
+//   }
+// });
 
-var app4 = new Vue({
-  el: '#app-4',
-  data: {
-    todos: [
-      { text: 'javascript 배우기'},
-      { text: 'vue 배우기' },
-      { text: '무언가 만들자' }
-    ]
-  },
-  methods: {
-    addList: function() {
-      this.todos.push({ text: 'test1' })
-    }
-  }
-});
+// var app4 = new Vue({
+//   el: '#app-4',
+//   data: {
+//     todos: [
+//       { text: 'javascript 배우기'},
+//       { text: 'vue 배우기' },
+//       { text: '무언가 만들자' }
+//     ]
+//   },
+//   methods: {
+//     addList: function() {
+//       this.todos.push({ text: 'test1' })
+//     }
+//   }
+// });
 
-var app5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: 'hello vue'
-  }
-});
+// var app5 = new Vue({
+//   el: '#app-5',
+//   data: {
+//     message: 'hello vue'
+//   }
+// });
+
+// Vue.component('todo-item', {
+//   props: ['todo'],
+//   template: '<li>{{todo.text}}</li>'
+// });
+
+// var app6 = new Vue({
+//   el: '#app-6',
+//   data: {
+//     groceryList: [
+//       { id: 0, text: 'Vegetables' },
+//       { id: 1, text: 'Cheese' },
+//       { id: 2, text: 'sefwefwef' }
+//     ]
+//   }
+// })
